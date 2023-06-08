ef0c261c1d195a8da8a71c9ad6ff831eec5fc448
Jaynimi <Eyeslikejays@yahoo.com>
2023-06-08 17:44:50 +0200

    Functionalities

---
+++ setup/src/Tours.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours}) => {
+  return <section>
+    <div className="title">
+      <h2>our tours</h2>
+      <div className="underline"></div>
+    </div>
+    <div className="">
+      {tours.map ((tour) => {
+        return <Tour key={tour.id} {...tour}></Tour>;
+      }
+      )}
+    </div>
+  </section>
+};
+ 
+export default Tours;
