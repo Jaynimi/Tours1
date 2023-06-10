ead9d3d956b28d4bf6b977d6047504916d4b0542
Jaynimi <Eyeslikejays@yahoo.com>
2023-06-10 15:08:19 +0200

    functionalities

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
+// Tour info api
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
   const [loading,setLoading] = useState(true);
   const [tours,setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
 
   const fetchTours = async () => {
     setLoading(true);
@@ -34,9 +39,21 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>
+            no tours left
+          </h2>
+          <button className='btn' onClick={() => fetchTours()}>Refresh</button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      < Tours tours = {tours} />
+      < Tours tours = {tours}  removeTour={removeTour}/>
     </main>
   );
 }
